068c476a278c1a2bcda776575fd01dcd60a7f717
prosper-codes <tpjn02@gmail.com>
2026-01-16 00:07:41 +0100

    modify network scanner

--- network_scanner/scanner.py
+++ network_scanner/scanner.py
@@ -6,9 +6,20 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+
     print("ip\t\t\tMAC Address\n----------------------------------------")
+    clients_list = []
     for item in answered_list:
-        print(item[1].psrc + "\t\t" + item[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
 
-scan("192.168.204/24")
+def display_results(results_list):
+    print("IP\t\t\tMAC Address \n")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+scanned_result = scan("192.168.204/24")
+display_results(scanned_result)
