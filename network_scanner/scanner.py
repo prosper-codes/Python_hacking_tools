478a399ffc83324f5762ca8f80fb944699cb2f82
prosper-codes <tpjn02@gmail.com>
2026-01-15 23:51:11 +0100

    improve netwrok scanner

--- network_scanner/scanner.py
+++ network_scanner/scanner.py
@@ -2,7 +2,13 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    print("ip\t\t\tMAC Address\n----------------------------------------")
+    for item in answered_list:
+        print(item[1].psrc + "\t\t" + item[1].hwsrc)
 
 
-scan("10.0.2.1")
+scan("192.168.204/24")
